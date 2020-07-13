5d1d9cc28900f5610996f74d670b2aeffc750f52
Linda Tonui <tonuilinda@gmail.com>
2020-07-13 15:15:58 +0300

    Add Procfile

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -20,7 +20,6 @@ public class App {
     public static void main(String[] args) {
         port(getHerokuAssignedPort());
         staticFileLocation("/public");
-        DB.sql2o.open();
 
         // home page
         get("/",((request, response) -> {
