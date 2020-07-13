600c3bb37fdf29efa5048d5f21b1926aa2bdbc6f
Linda Tonui <tonuilinda@gmail.com>
2020-07-13 16:20:29 +0300

    add

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -8,6 +8,6 @@ import java.sql.SQLException;
 public class DB{
 
     //public static Sql2o sql2o =new Sql2o("jdbc:postgresql://localhost:5432/wildlifetracker", "tonui", "chepkemoi1999.");
-    public  static  Sql2o sql2o = new Sql2o("postgresql://fmxnigckrhbeli:4d4b52348b6097398409e2cf56a49840cc0c0fe502909404ab8668ff34bc5595@ec2-34-225-162-157.compute-1.amazonaws.com:5432/d1uscsdjn4eh6f","fmxnigckrhbeli","4d4b52348b6097398409e2cf56a49840cc0c0fe502909404ab8668ff34bc5595");
+    public  static  Sql2o sql2o = new Sql2o("jdbc:postgresql://fmxnigckrhbeli:4d4b52348b6097398409e2cf56a49840cc0c0fe502909404ab8668ff34bc5595@ec2-34-225-162-157.compute-1.amazonaws.com:5432/d1uscsdjn4eh6f","fmxnigckrhbeli","4d4b52348b6097398409e2cf56a49840cc0c0fe502909404ab8668ff34bc5595");
 
 }
